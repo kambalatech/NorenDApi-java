77eb2f9cc02b4ffd7326a9f14dd55e4fa072b677
Annappaacharya <annappa.achar@kambala.co.in>
2026-07-16 16:40:49 +0530

    exchange message api call added

--- src/main/java/com/kspl/norenexampleapp/NorenDExampleApp.java
+++ src/main/java/com/kspl/norenexampleapp/NorenDExampleApp.java
@@ -20,41 +20,45 @@ public class NorenDExampleApp {
         System.out.println("Hello and Welcome to Noren!");
         NorenDApiJava api = new com.noren.javaapi.NorenDApiJava("https://rama.kambala.co.in/DealerWClient/");
         
-        String response = api.login("TSTDLR1", "Abc@123", "RRRRR1997R", "Krishna_DAPI", "krishpernad123", "java-");
+        String response = api.login("ANNAPPADLR", "Abc@123", "RRRRR1997R", "Krishna_DAPI", "krishpernad123", "java-");
         System.out.println(response);
             
         
-        JSONObject securityinfo=api.get_security_info("NFO","335003");
-        System.out.println(securityinfo.toString());
+        // JSONObject securityinfo=api.get_security_info("NFO","335003");
+        // System.out.println(securityinfo.toString());
 
         
-        JSONObject reply = api.place_order("B","C","TSTINV3", "NSE", "CANBK-EQ", 1, 0, "LMT", 220.0, "java", null, null, null, null, null, null, null, null); 
-        System.out.println(reply.toString());
+        // JSONObject reply = api.place_order("B","C","TSTINV3", "NSE", "CANBK-EQ", 1, 0, "LMT", 220.0, "java", null, null, null, null, null, null, null, null); 
+        // System.out.println(reply.toString());
         
-        //String newprice_type,Double newprice,Double newtrigger_price,Double bookloss_price, Double bookprofit_price , Double trail_price)
-        JSONObject test= api.modify_order("22122800000006","NSE","AARVI-EQ",50,"DS",610.0, 0.0, 0.0 ,0.0 , 0.0,"TSTINV3"); 
-        System.out.println(test.toString());
+        // //String newprice_type,Double newprice,Double newtrigger_price,Double bookloss_price, Double bookprofit_price , Double trail_price)
+        // JSONObject test= api.modify_order("22122800000006","NSE","AARVI-EQ",50,"DS",610.0, 0.0, 0.0 ,0.0 , 0.0,"TSTINV3"); 
+        // System.out.println(test.toString());
         
-        JSONArray book; 
-        book = api.get_order_book();
-        System.out.println(book.toString());
+        // JSONArray book; 
+        // book = api.get_order_book();
+        // System.out.println(book.toString());
         
-        book = api.get_trade_book(); 
-        if(book != null)
-            System.out.println(book.toString());
+        // book = api.get_trade_book(); 
+        // if(book != null)
+        //     System.out.println(book.toString());
         
-        JSONObject exitorder = api.exit_order("22122700000001","H"); 
-        System.out.println(exitorder.toString());
+        // JSONObject exitorder = api.exit_order("22122700000001","H"); 
+        // System.out.println(exitorder.toString());
         
-        JSONObject cancelorder = api.cancel_order("22122700000001"); 
-        System.out.println(cancelorder.toString());
+        // JSONObject cancelorder = api.cancel_order("22122700000001"); 
+        // System.out.println(cancelorder.toString());
         
-        JSONObject quotes = api.get_quotes("NSE","2211"); 
-        System.out.println(quotes.toString());
+        // JSONObject quotes = api.get_quotes("NSE","2211"); 
+        // System.out.println(quotes.toString());
         
-        JSONObject clients = api.get_clients(); 
-        System.out.println(clients.toString());
+        // JSONObject clients = api.get_clients(); 
+        // System.out.println(clients.toString());
         
+        
+        JSONArray exchmsg = api.get_exchmsg(); 
+        System.out.println(exchmsg.toString());
+
         JSONArray pos;
         pos = api.get_positions(); 
         System.out.println(pos.toString());
